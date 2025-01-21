ab6adb9fc205a897392bff45ed63e0ec6535d08b
권정우 <kiw970923@naver.com>
2025-01-21 10:36:21 +0900

    CHORE: SourceTree Commit 테스트

--- devoot-backend/src/main/java/com/gamee/devoot_backend/HelloController.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/HelloController.java
@@ -3,6 +3,9 @@ package com.gamee.devoot_backend;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Hello Test
+ */
 @RestController
 public class HelloController {
     @GetMapping("/hello")
