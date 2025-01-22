82ed0b78b2a5f04d049d1948d13554a6d9263de4
권정우 <kiw970923@naver.com>
2025-01-22 12:51:23 +0900

    FEAT: 강의 상세 내용 GET 요청 기능
    - bookmark 기능 추가 시 강의 상세 정보에 찜한 횟수 쿼리 추가
    
    jira ref: #S12P11A209-163

--- devoot-backend/src/main/java/com/gamee/devoot_backend/lecture/service/LectureService.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/lecture/service/LectureService.java
@@ -5,15 +5,30 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gamee.devoot_backend.lecture.dto.LectureDetail;
 import com.gamee.devoot_backend.lecture.entity.Lecture;
 import com.gamee.devoot_backend.lecture.repository.LectureRepository;
+import com.gamee.devoot_backend.lecturereview.repository.LectureReviewRepository;
 
 @Service
 public class LectureService {
 	@Autowired
 	private LectureRepository lectureRepository;
-	public Lecture getLectureDetail(long id) {
-		Optional<Lecture> lecture = lectureRepository.findById(id);
-		return lecture.orElse(null);
+	@Autowired
+	private LectureReviewRepository lectureReviewRepository;
+
+	public LectureDetail getLectureDetail(long id) {
+		Optional<Lecture> lectureOptional = lectureRepository.findById(id);
+		if (lectureOptional.isPresent()) {
+			Lecture lecture = lectureOptional.get();
+			Float rating = lectureReviewRepository.findAvgByLectureId(lecture.getId());
+			if (rating == null) {
+				rating = 0f;
+			}
+			// 강의 찜하기 기능 구현 시 찜 개수 불러올 수 있도록 변경 필요
+			//LectureDetail lectureDetail = new LectureDetail(lecture, BookmarkRepository.countByLectureId(lecture.getId()), rating);
+			return new LectureDetail(lecture, 0, rating);
+		}
+		return null;
 	}
 }
