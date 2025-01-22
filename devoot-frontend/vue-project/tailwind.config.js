24bd6fda842ff106801170d07af0243f2d57253d
seungju <seungju425@gmail.com>
2025-01-22 14:51:55 +0900

    FEAT: tailwind 커스텀 설정 및 .gitignore 수정
    - tailwind.config.js 색 커스텀
    - input.css 폰트 사이즈 클래스 생성
    - index.html 예시코드 추가(확인용)
    - .gitignore에 out.css 제외 코드 추가
    
    jira ref: #S12P11A209-167

--- devoot-frontend/vue-project/tailwind.config.js
+++ devoot-frontend/vue-project/tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#CDE3FF',
+          200: '#9AC7FF',
+          300: '#68AAFF',
+          400: '#358EFF',
+          500: '#0372FF',
+        },
+        gray: {
+          100: '#F4F6F8',
+          200: '#E8EBEE',
+          300: '#9DA0A3',
+          400: '#75787B',
+          500: '#25282B',
+        },
+      },
+    },
   },
   plugins: [],
 }
