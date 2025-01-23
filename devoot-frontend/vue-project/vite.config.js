da43d00e0bdc5a551e3a4626d2938487b2b0ab79
nahyun <irene.k2547@gmail.com>
2025-01-23 18:01:57 +0900

    CHORER: vite-svg-loader 설정정
    - 아이콘 색 변경 불가 오류 해결을 위해 vite-svg-loader 설치
    - vite.config.js 설정 수정
    - logo.svg 코드 단순화화
    
    jira ref: #S12P11A209-206

--- devoot-frontend/vue-project/vite.config.js
+++ devoot-frontend/vue-project/vite.config.js
@@ -4,12 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from 'tailwindcss'
+import svgLoader from 'vite-svg-loader' //SVG Loader 가져오기기
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    svgLoader(),
   ],
   resolve: {
     alias: {
