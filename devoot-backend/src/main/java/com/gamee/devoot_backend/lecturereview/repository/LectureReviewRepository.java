d13deb471ead4ac7b1d4e2c92a414eb1c161f3a8
SSAFY <spig0126@naver.com>
2025-01-24 15:58:04 +0900

    REMOVE: incomplete files

--- devoot-backend/src/main/java/com/gamee/devoot_backend/lecturereview/repository/LectureReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gamee.devoot_backend.lecturereview.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gamee.devoot_backend.lecturereview.entity.LectureReview;
-
-public interface LectureReviewRepository extends JpaRepository<Long, LectureReview> {
-
-}
