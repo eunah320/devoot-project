18307d904098a5a62a9744652a21f271aaf72d17
nahyun <irene.k2547@gmail.com>
2025-01-27 17:24:00 +0900

    DESIGN: LogInPage 퍼블리싱
    - LogInPage HTML, CSS 작성
    - LogInPage 반응형 설정
    
    jira ref: #S12P11A209-200

--- devoot-frontend/vue-project/src/views/LogInPage.vue
+++ devoot-frontend/vue-project/src/views/LogInPage.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>
-        signin
+    <div
+        class="flex flex-col col-span-12 gap-6 p-16 border border-gray-200 lg:col-span-6 rounded-2xl"
+    >
+        <!-- 로고 -->
+        <div id="logo" class="flex flex-row items-center gap-3">
+            <Logo class="w-8 h-8" />
+            <div class="text-h1">개발바닥</div>
+        </div>
+
+        <!-- 멘트 -->
+        <div class="text-gray-300 text-h2">
+            개발의 모든 것, <br />
+            개발바닥에서 시작해보세요!
+        </div>
+
+        <!-- 소셜 로그인 버튼 -->
+        <div class="flex flex-row gap-4">
+            <button
+                class="flex flex-row items-center justify-center w-full h-12 gap-4 text-black border border-gray-200 rounded-lg"
+            >
+                <Google class="w-6 h-6" />
+                <span class="text-body-bold">Google로 로그인</span>
+            </button>
+        </div>
+        <div class="flex flex-row gap-4">
+            <button
+                class="flex flex-row items-center justify-center w-full h-12 gap-4 text-black border border-gray-200 rounded-lg"
+            >
+                <Github class="w-6 h-6" />
+                <span class="text-body-bold">GitHub로 로그인</span>
+            </button>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+import Logo from '@/assets/icons/logo.svg'
+import Google from '@/assets/icons/google.svg'
+import Github from '@/assets/icons/github.svg'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
