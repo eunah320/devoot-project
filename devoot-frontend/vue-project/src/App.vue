6f09e430c3311216d6eb2f5d3ae4a60cd3904d7a
nahyun <irene.k2547@gmail.com>
2025-01-27 13:53:47 +0900

    DESIGN: AppNavigation 퍼블리싱
    - AppNavigation HTML, CSS 코드 작성
    - AppNavigation 반응형 구현
    - update_profile.svg 코드 수정
    - App.vue에 Navigation import
    
    jira ref: #S12P11A209-173

--- devoot-frontend/vue-project/src/App.vue
+++ devoot-frontend/vue-project/src/App.vue
@@ -1,14 +1,29 @@
 <template>
-    <div>
-        <AppNavigation />
+    <div class="flex flex-row">
+        <!-- 네비게이션 바 -->
+        <AppNavigation class="fixed" @menu-selected="handleMenuSelected" />
+
+        <!-- 화면 -->
+        <div
+            id="container"
+            class="grid ml-[4.5rem] lg:ml-[13.5rem] flex-1 grid-cols-12 gap-6 overflow-y-auto px-9"
+        >
+            <!-- 선택된 메뉴에 따라 화면 전환 부분을 구현 -->
+        </div>
     </div>
 </template>
 
 <script setup>
-import AppNavigation from './components/layout/AppNavigation.vue';
+import { ref } from 'vue'
+import AppNavigation from './components/layout/AppNavigation.vue'
 
+const selectedMenu = ref('home')
+
+// 메뉴가 선택되었을 때 호출되는 함수
+const handleMenuSelected = (menu) => {
+    selectedMenu.value = menu
+    console.log('Selected Menu:', menu) // 선택된 메뉴를 출력
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- devoot-frontend/vue-project/src/components/layout/AppNavigation.vue
+++ devoot-frontend/vue-project/src/components/layout/AppNavigation.vue
@@ -1,52 +1,96 @@
 <template>
-    <div id="navigation" class="w-[13.5rem] flex flex-col h-screen bg-white border-r border-gray-200">
+    <div
+        id="navigation"
+        class="w-[4.5rem] lg:w-[13.5rem] flex flex-col h-screen bg-white border-r ease-in-out border-gray-200 transition-all lg:transition-transform duration-500"
+    >
         <!-- 로고 -->
-    <div id="logo" class="flex flex-row">
-        <div class="w-full text-h1">개발바닥</div>
-    </div>
+        <div id="logo" class="flex flex-row items-center justify-center h-20 gap-3">
+            <Logo class="w-8 h-8" />
+            <div class="hidden text-h1 lg:block">개발바닥</div>
+        </div>
 
-    <!-- 메뉴 -->
-    <div id="menu-container" class="flex flex-col flex-1 py-3">
-        <!-- 서비스 관련 메뉴 -->
-        <div id="menu">
-            <div class="w-full py-4 px-9">
-                <p class="text-left text-body">홈</p>
+        <!-- 메뉴 -->
+        <div id="menu-container" class="flex flex-col flex-1 py-3">
+            <!-- 서비스 관련 메뉴 -->
+            <div id="menu">
+                <div
+                    v-for="(menu, index) in serviceMenu"
+                    :key="index"
+                    class="flex flex-row items-center justify-center gap-5 px-0 py-4 cursor-pointer lg:justify-start lg:px-9"
+                    :class="selectedMenu === menu.name ? 'text-black' : 'text-gray-300'"
+                    @click="selectMenu(menu.name)"
+                >
+                    <component
+                        :is="selectedMenu === menu.name ? menu.iconFill : menu.iconDefault"
+                        class="w-6 h-6"
+                    />
+                    <p class="hidden text-left text-body lg:block">
+                        {{ menu.label }}
+                    </p>
+                </div>
             </div>
-            <div class="w-full py-4 px-9">
-                <p class="text-left text-body">강의</p>
-            </div>
-            <div class="w-full py-4 px-9">
-                <p class="text-left text-body">타임라인</p>
-            </div>
-            <div class="w-full py-4 px-9">
-                <p class="text-left text-body">프로필</p>
+
+            <!-- 간격용 -->
+            <div class="flex-1"></div>
+
+            <!-- 회원 관련 메뉴-->
+            <div id="member-menu">
+                <div
+                    v-for="(menu, index) in memberMenu"
+                    :key="index"
+                    class="flex flex-row items-center justify-center gap-5 px-0 py-4 cursor-pointer lg:justify-start lg:px-9"
+                    :class="selectedMenu === menu.name ? 'text-black' : 'text-gray-300'"
+                    @click="selectMenu(menu.name)"
+                >
+                    <component :is="menu.icon" class="w-6 h-6" />
+                    <p class="hidden text-left text-body lg:block">{{ menu.label }}</p>
+                </div>
             </div>
         </div>
-        
-        <!-- 간격용 -->
-        <div class="flex-1"></div>
-
-        <!-- 회원 관련 메뉴-->
-        <div id="member-menu">
-            <div class="w-full py-4 px-9">
-                <p class="text-left text-body">로그인/회원가입</p>
-            </div>
-            <div class="w-full py-4 px-9">
-                <p class="text-left text-body">회원정보 수정</p>
-            </div>
-            <div class="w-full py-4 px-9">
-                <p class="text-left text-body">로그아웃</p>
-            </div>
-        </div>
     </div>
-    </div>
-    
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import Logo from '@/assets/icons/logo.svg'
+import HomeDefault from '@/assets/icons/home_default.svg'
+import HomeFill from '@/assets/icons/home_fill.svg'
+import LectureDefault from '@/assets/icons/lecture_default.svg'
+import LectureFill from '@/assets/icons/lecture_fill.svg'
+import TimelineDefault from '@/assets/icons/timeline_default.svg'
+import TimelineFill from '@/assets/icons/timeline_fill.svg'
+import ProfileDefault from '@/assets/icons/profile_default.svg'
+import ProfileFill from '@/assets/icons/profile_fill.svg'
+import LogIn from '@/assets/icons/login.svg'
+import UpdateProfile from '@/assets/icons/update_profile.svg'
+import LogOut from '@/assets/icons/logout.svg'
 
+// 메뉴를 App.vue로 전달하기 위한 이벤트 정의
+const emmit = defineEmits(['menu-selected'])
+
+// 선택된 메뉴를 저장하는 변수 (기본값: 'home')
+const selectedMenu = ref('home')
+
+// 메뉴를 선택했을 때 호출되는 함수
+const selectMenu = (menu) => {
+    selectedMenu.value = menu
+    emmit('menu-selected', menu)
+}
+
+// 서비스 메뉴 항목 배열
+const serviceMenu = [
+    { name: 'home', label: '홈', iconDefault: HomeDefault, iconFill: HomeFill },
+    { name: 'lecture', label: '강의', iconDefault: LectureDefault, iconFill: LectureFill },
+    { name: 'timeline', label: '타임라인', iconDefault: TimelineDefault, iconFill: TimelineFill },
+    { name: 'profile', label: '프로필', iconDefault: ProfileDefault, iconFill: ProfileFill },
+]
+
+// 회원 관련 메뉴 항목 배열
+const memberMenu = [
+    { name: 'login', label: '로그인/회원가입', icon: LogIn },
+    { name: 'edit-profile', label: '회원정보 수정', icon: UpdateProfile },
+    { name: 'logout', label: '로그아웃', icon: LogOut },
+]
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
