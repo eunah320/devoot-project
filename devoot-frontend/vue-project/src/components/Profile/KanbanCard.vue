fffd01c0a0ae3d967f23ed7bb49465b9012a4661
seungju <seungju425@gmail.com>
2025-01-28 12:41:13 +0900

    FEAT: 칸반 카드 제작 완료
    - 칸반 카드 더미 데이터 생성
    - move 버튼 호버 시 커서 변경
    - 강의명 2줄 이상 말줄임표, 호버시 전체 강의명
    - 카드 높이 5rem->6rem 변경
    - 태그 caption-sm으로 변경
    
    jira ref: #S12P11A209-185

--- devoot-frontend/vue-project/src/components/Profile/KanbanCard.vue
+++ devoot-frontend/vue-project/src/components/Profile/KanbanCard.vue
@@ -1,30 +1,42 @@
 <template>
-    <div class="flex w-[20.5rem] h-20 border border border-black rounded-lg overflow-hidden">
+    <div
+        class="bg-white flex w-[20.5rem] h-[6rem] border border-gray-200 rounded-lg overflow-hidden"
+        v-for="lecture in lectureDatas"
+        :key="lecture.id"
+    >
         <!-- Thumbnail Container -->
-        <div class="w-[7.5rem] h-full bg-gray-300">Thunbnail container</div>
+        <div class="w-[7.5rem] h-full bg-gray-300 flex-shrink-0 relative">
+            <img :src="lecture.imageUrl" alt="" class="w-full h-full" />
+            <Move class="absolute w-6 h-6 text-white top-[33.6px] cursor-move" />
+        </div>
 
         <!-- Info Container -->
-        <div class="flex flex-col items-center justify-center w-full h-full gap-2 px-3">
+        <div class="flex flex-col w-full h-full gap-2 px-3 py-2">
             <!-- Title Section -->
-            <div class="flex w-full gap-x-0.5">
-                <div class="w-full">
-                    <p class="text-gray-400 text-caption-sm">인프런</p>
-                    <p class="text-black text-body">인프런</p>
+            <div class="flex items-center justify-between w-full h-full gap-x-0.5">
+                <div class="flex flex-col justify-center w-full h-full">
+                    <p class="text-gray-400 text-caption-sm">{{ lecture.siteName }}</p>
+                    <p
+                        class="text-black text-overflow text-body cursor-text"
+                        :title="lecture.courseName"
+                    >
+                        {{ lecture.courseName }}
+                    </p>
                 </div>
-                <div class="flex items-center">
+                <div class="flex">
                     <BookmarkFilled class="w-6 h-6 text-primary-500" />
                 </div>
             </div>
             <!-- Tag Section -->
-            <div class="inline-flex w-full gap-1.5">
-                <span class="inline-flex gap-1 px-2 py-1 tag-gray">
+            <div class="flex flex-wrap gap-1.5">
+                <div
+                    class="inline-flex gap-1 text-caption-sm tag-gray"
+                    v-for="tag in lecture.tags"
+                    :key="tag"
+                >
                     <p>#</p>
-                    <p>태그1</p>
-                </span>
-                <span class="inline-flex gap-1 px-2 py-1 tag-gray">
-                    <p>#</p>
-                    <p>태그2</p>
-                </span>
+                    <p>{{ tag }}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -32,6 +44,31 @@
 
 <script setup>
 import BookmarkFilled from '@/assets/icons/bookmark_filled.svg'
+import Move from '@/assets/icons/move.svg'
+import { ref, onMounted } from 'vue'
+
+const lectureDatas = ref([])
+
+const loadLectureDatas = async () => {
+    const response = await fetch('./kanbancard_dummy_data.json')
+    const data = await response.json()
+    lectureDatas.value = data
+    console.log('강의 데이터', lectureDatas.value)
+}
+
+onMounted(() => {
+    loadLectureDatas() // 컴포넌트가 로드될 때 JSON 데이터 가져오기
+})
 </script>
 
-<style></style>
+<style>
+.text-overflow {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    word-break: break-word;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+}
+</style>
