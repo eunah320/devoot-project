4d450ab8b3dd6634547b290825c81cebc58084ad
정연희 <spig0126@gmail.com>
2025-01-31 00:36:32 +0900

    CHORE: add forgotten file for 'get todo contribution list API'
    - add TodoContributionDetailDto
    
    jira ref: ##S12P11A209-161

---
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/todo/dto/TodoContributionDetailDto.java
@@ -0,0 +1,20 @@
+package com.gamee.devoot_backend.todo.dto;
+
+import java.time.LocalDate;
+
+import com.gamee.devoot_backend.todo.entity.TodoContribution;
+
+import lombok.Builder;
+
+@Builder
+public record TodoContributionDetailDto (
+	Integer cnt,
+	LocalDate date
+){
+	public static TodoContributionDetailDto of(TodoContribution todoContribution) {
+		return TodoContributionDetailDto.builder()
+			.cnt(todoContribution.getCnt())
+			.date(todoContribution.getDate())
+			.build();
+	}
+}
