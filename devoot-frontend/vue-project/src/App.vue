b5ceaed6f0fd928aa0403c48a3f9492d581a46b1
eunah <eunah320@naver.com>
2025-01-31 02:03:12 +0900

    FEAT: 헤더 카테테고리 버튼 수정 - 카테고리 버튼 드랍다운 변경 - 카테고리 드랍다운 브랜치 준비
    jira ref: #S12P11A209-174

--- devoot-frontend/vue-project/src/App.vue
+++ devoot-frontend/vue-project/src/App.vue
@@ -6,7 +6,7 @@
         <!-- 화면 컨테이너 -->
         <div class="flex flex-col flex-1 ml-[4.5rem] lg:ml-[13.5rem]">
             <!-- 헤더 -->
-            <AppHeader :type="headerType" />
+            <AppHeader v-if="shouldShowHeader" :type="headerType" />
 
             <!-- 본문 -->
             <div id="container" class="grid flex-1 grid-cols-12 gap-6 overflow-y-auto px-9">
@@ -22,8 +22,10 @@ import { useRoute } from 'vue-router'
 import AppNavigation from './components/layout/AppNavigation.vue'
 import AppHeader from './components/layout/AppHeader.vue'
 
-// 현재 경로에 따라 헤더 타입 결정
+// 현재 경로 가져오기
 const route = useRoute()
+
+// 현재 경로에 따라 헤더 타입 결정
 const headerType = computed(() => {
     const path = route.path
     if (path === '/' || path.startsWith('/lecture')) {
@@ -33,10 +35,22 @@ const headerType = computed(() => {
     }
     return 'default'
 })
+
+// 특정 경로에서 헤더 숨기기
+const shouldShowHeader = computed(() => {
+    const hiddenPaths = ['/login', '/profile/:id/edit'] // 숨길 경로
+    const currentPath = route.path
+
+    // 동적 경로 매칭 처리
+    return !hiddenPaths.some((hiddenPath) => {
+        const regex = new RegExp(`^${hiddenPath.replace(':id', '[^/]+')}$`) // ":id"를 정규식으로 변환
+        return regex.test(currentPath)
+    })
+})
 </script>
 
 <style scoped>
 #container {
-    min-height: calc(100vh - h-20); /* 헤더 높이를 제외한 영역 */
+    min-height: calc(100vh - 5rem); /* 헤더 높이를 제외한 영역 */
 }
 </style>
