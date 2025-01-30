cfc1622ba9660c37fed1a3c937b71988111bc1c2
eunah <eunah320@naver.com>
2025-01-31 01:33:26 +0900

    FEAT: 카테고리 드랍다운
    - 내용 구성 완료
    - 선택시 카테고리별 필터링 필요요
    
    jira ref: #S12P11A209-193

--- devoot-frontend/vue-project/src/components/Common/CategoryDropDown.vue
+++ devoot-frontend/vue-project/src/components/Common/CategoryDropDown.vue
@@ -1,7 +1,39 @@
 <template>
-    <div>드랍다운이어요</div>
+    <ul class="bg-white border rounded shadow-lg w-[10.75rem]">
+        <li
+            v-for="(category, index) in categories"
+            :key="index"
+            class="flex items-center justify-start w-full h-[2.25rem] px-4 text-body text-gray-700 hover:bg-gray-100 cursor-pointer"
+            @click="selectCategory(category)"
+        >
+            {{ category }}
+        </li>
+    </ul>
 </template>
 
-<script setup></script>
+<script setup>
+// 카테고리 목록
+const categories = [
+    '백엔드',
+    '프론트엔드',
+    '모바일 앱 개발',
+    '프로그래밍 언어',
+    '알고리즘 / 자료구조',
+    '데이터베이스',
+    '테브옵스 / 인프라',
+    '게임 개발',
+    '데이터 사이언스',
+    '인공지능',
+    '보안 / 네트워크',
+    '하드웨어',
+    '디자인 / 아트',
+    '기타',
+]
 
-<style lang="scss" scoped></style>
+// 선택된 카테고리를 처리하는 함수
+const selectCategory = (category) => {
+    console.log(`선택된 카테고리: ${category}`)
+}
+</script>
+
+<style scoped></style>
