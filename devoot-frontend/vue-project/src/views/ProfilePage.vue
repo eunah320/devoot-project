8cd95ed282affd609bfefe394ec0f76197e5009c
nahyun <irene.k2547@gmail.com>
2025-02-03 17:53:20 +0900

    FEAT: 탭 메뉴 컴포넌트 완성성
    - TabMenu.vue에서 props를 활용하여 동적으로 탭 이름을 설정할 수 있도록 구현현
    - 선택한 탭에 따라 부모 컴포넌트에서 다른 컴포넌트를 렌더링하도록 이벤트(`updateTab`) 추가
    
    jira ref: #S12P11A209-190

--- devoot-frontend/vue-project/src/views/ProfilePage.vue
+++ devoot-frontend/vue-project/src/views/ProfilePage.vue
@@ -1,13 +1,29 @@
 <template>
     <div>
         profile
+        <TabMenu tab-left="북마크한 강의" tab-right="내가 쓴 리뷰" @update-tab="handleTabChange" />
+        <!-- <component :is="currentComponent" /> -->
     </div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
+import TabMenu from '@/components/Common/TabMenu.vue'
+// import KanbanSection from '@/components/Profile/KanbanSection.vue'
+// import ProfileReviewSection from '@/components/Profile/ProfileReviewSection.vue'
 
+// 현재 선택된 탭 (기본값 : 'left')
+const currentTab = ref('left')
+
+// 탭에 따라 렌더링할 컴포넌트 설정
+// const currentComponent = computed(() => {
+//     return currentTab.value === 'left' ? KanbanSection : ProfileReviewSection
+// })
+
+// 탭 변경 이벤트 핸들러
+const handleTabChange = (tab) => {
+    currentTab.value = tab
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
