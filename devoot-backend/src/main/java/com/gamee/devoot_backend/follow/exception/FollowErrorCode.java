d80cc4d84a8052ab6ea696ecc3acffcb319ac6f2
김본 <qhsl0615@naver.com>
2025-02-09 01:48:52 +0900

    FEAT: FollowRequestAlreadyAcceptedException 생성
    - FOLLOW_REQUEST_ALREADY_ACCEPTED 에러 코드 생성
    - FollowRequestAlreadyAcceptedException 클래스 생성
    
    jira ref: #S12P11A209-52

--- devoot-backend/src/main/java/com/gamee/devoot_backend/follow/exception/FollowErrorCode.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/follow/exception/FollowErrorCode.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum FollowErrorCode implements ErrorCode {
 	FOLLOW_RELATIONSHIP_ALREADY_EXISTS(HttpStatus.CONFLICT, "FOLLOW_409_1", "Already following this user."),
+	FOLLOW_REQUEST_ALREADY_ACCEPTED(HttpStatus.CONFLICT, "FOLLOW_409_2", "This follow request has already been accepted."),
 	FOLLOW_CANNOT_FOLLOW_SELF(HttpStatus.BAD_REQUEST, "FOLLOW_400_1", "Can't follow yourself."),
 	FOLLOW_RELATIONSHIP_NOT_FOUND(HttpStatus.NOT_FOUND, "FOLLOW_404_1", "Follow relationship does not exist."),
 	FOLLOW_REQUEST_PENDING(HttpStatus.FORBIDDEN, "FOLLOW_403_1", "User's follow request is still pending approval"),
