bdd3ee3ec0ca63862e9a9dea0eb0a9e2c38d44f0
정연희 <spig0126@gmail.com>
2025-02-10 00:35:03 +0900

    FEAT: add timeline join fetch queries in repositories
    jira ref: ##S12P11A209-161

--- devoot-backend/src/main/java/com/gamee/devoot_backend/bookmark/entity/BookmarkLog.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/bookmark/entity/BookmarkLog.java
@@ -3,10 +3,13 @@ package com.gamee.devoot_backend.bookmark.entity;
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gamee.devoot_backend.bookmark.dto.BookmarkLogDetailDto;
@@ -29,7 +32,7 @@ import lombok.experimental.SuperBuilder;
 @DiscriminatorValue("BOOKMARK")
 @EqualsAndHashCode(callSuper = true)
 public class BookmarkLog extends TimelineLog {
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "lectureId", insertable = false, updatable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
 	private Lecture lecture;
 	private long lectureId;
