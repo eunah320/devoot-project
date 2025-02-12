f0ee8b54abcda18d9593df37197e235aebb09182
SSAFY <spig0126@naver.com>
2025-02-12 13:50:58 +0900

    MERGE: be-feature/251-timeline - be-dev (view followed user's timeline logs)
    jira-ref: #S12P11A209-251

--- devoot-backend/src/test/java/com/gamee/devoot_backend/timeline/repository/TimelineLogRepositoryTest.java
+++ devoot-backend/src/test/java/com/gamee/devoot_backend/timeline/repository/TimelineLogRepositoryTest.java
@@ -1,8 +1,6 @@
 package com.gamee.devoot_backend.timeline.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 
@@ -20,7 +18,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gamee.devoot_backend.bookmark.entity.BookmarkLog;
 import com.gamee.devoot_backend.bookmark.repository.BookmarkLogRepository;
-import com.gamee.devoot_backend.bookmark.repository.BookmarkRepository;
+import com.gamee.devoot_backend.follow.entity.Follow;
+import com.gamee.devoot_backend.follow.repository.FollowRepository;
 import com.gamee.devoot_backend.lecture.entity.Lecture;
 import com.gamee.devoot_backend.lecture.repository.LectureRepository;
 import com.gamee.devoot_backend.timeline.entity.TimelineLog;
@@ -47,7 +46,7 @@ public class TimelineLogRepositoryTest {
 	UserRepository userRepository;
 
 	@Autowired
-	BookmarkRepository bookmarkRepository;
+	FollowRepository followRepository;
 
 	@Autowired
 	BookmarkLogRepository bookmarkLogRepository;
@@ -66,13 +65,18 @@ public class TimelineLogRepositoryTest {
 	public void testFindById() {
 		// Given
 		final User user = User.builder().uid("1234").build();
+		final User userFriend = User.builder().uid("12345").build();
 		userRepository.save(user);
+		userRepository.save(userFriend);
 
 		final Lecture lecture = Lecture.builder().build();
 		lectureRepository.save(lecture);
 
+		Follow follow = Follow.builder().followerId(user.getId()).followedId(userFriend.getId()).allowed(true).build();
+		followRepository.save(follow);
+
 		final Todo todo = Todo.builder()
-			.userId(user.getId())
+			.userId(userFriend.getId())
 			.date(LocalDate.now())
 			.lectureId(lecture.getId())
 			.lectureName("Lecture")
@@ -85,7 +89,7 @@ public class TimelineLogRepositoryTest {
 
 		final TodoLog todoLog = TodoLog.builder()
 			.todoId(todo.getId())
-			.userId(user.getId())
+			.userId(userFriend.getId())
 			.build();
 		todoLogRepository.save(todoLog);
 
@@ -93,7 +97,7 @@ public class TimelineLogRepositoryTest {
 			.beforeStatus(null)
 			.afterStatus(1)
 			.lectureId(lecture.getId())
-			.userId(user.getId())
+			.userId(userFriend.getId())
 			.build();
 		bookmarkLogRepository.save(bookmarkLog);
 
@@ -111,11 +115,9 @@ public class TimelineLogRepositoryTest {
 				String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(log);
 				System.out.println(prettyJson);
 
-				if (log instanceof TodoLog) {
-					TodoLog todoLog1 = (TodoLog) log;
+				if (log instanceof TodoLog todoLog1) {
 					assertEquals(todo.getId(), todoLog1.getTodoId(), "TodoLog should match the created todo");
-				} else if (log instanceof BookmarkLog) {
-					BookmarkLog bookmarkLog1 = (BookmarkLog) log;
+				} else if (log instanceof BookmarkLog bookmarkLog1) {
 					assertNotNull(bookmarkLog1.getAfterStatus(), "After status should not be null");
 					assertEquals(1, bookmarkLog1.getAfterStatus(), "After status should match expected value");
 				}
