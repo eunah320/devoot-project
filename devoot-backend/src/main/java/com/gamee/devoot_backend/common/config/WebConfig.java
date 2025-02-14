80619290af3f8536fa263dfbc485bc45ac6ae160
권정우 <kiw970923@naver.com>
2025-02-14 12:46:21 +0900

    FIX: CORS 허용 origin에 backend 서버 주소 추가

--- devoot-backend/src/main/java/com/gamee/devoot_backend/common/config/WebConfig.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/common/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("http://localhost:3000")
-			.allowedOrigins("https://devoot-frontend:3000")
+			.allowedOrigins("http://i12a209.p.ssafy.io")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true);
