8eeb22c00700276228bf50326d31224de8accb5f
nahyun <irene.k2547@gmail.com>
2025-02-17 13:33:39 +0900

    CHORE: 포트 설정 변경
    jira ref: #S12P11A209-306

--- devoot-frontend/admin/vite.config.js
+++ devoot-frontend/admin/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-import svgLoader from 'vite-svg-loader' //SVG Loader 가져오기
+import svgLoader from 'vite-svg-loader' // SVG Loader 가져오기
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +13,10 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
+    },
+    server: {
+        port: 3000, // ✅ 포트 설정을 server 객체 내부에 추가
+        strictPort: true, // (선택) 포트가 사용 중이면 실행 중지 (자동 변경 방지)
+        host: true, // (선택) LAN에서 접속 가능하도록 설정 (기본값: 'localhost')
     },
 })
