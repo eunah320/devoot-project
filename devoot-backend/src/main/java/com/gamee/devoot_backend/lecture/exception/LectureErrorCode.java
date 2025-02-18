fad043146a761569ca003c6a6c6080231bdbe65c
김본 <qhsl0615@naver.com>
2025-02-18 09:56:55 +0900

    REFACTOR: 불필요한 검색 쿼리 유효성 검사 예외 코드 제거
    - `SEARCH_QUERY_INVALID` 오류 코드 삭제 (SEARCH_400_1)
    
    jira ref: #S12P11A209-40

--- devoot-backend/src/main/java/com/gamee/devoot_backend/lecture/exception/LectureErrorCode.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/lecture/exception/LectureErrorCode.java
@@ -14,7 +14,6 @@ public enum LectureErrorCode implements ErrorCode {
 	PRIVATE_ACCOUNT_ACCESS_DENIED(HttpStatus.FORBIDDEN, "LECTURE_403_1", "You do not have permission to access this user"),
 	LECTURE_NOT_EXIST(HttpStatus.NOT_FOUND, "LECTURE_404_1", "Lecture does not exist."),
 	USER_ALREADY_REPORTED(HttpStatus.CONFLICT, "LECTURE_409_1", "User already reported this lecture"),
-	SEARCH_QUERY_INVALID(HttpStatus.BAD_REQUEST, "SEARCH_400_1", "The search query is invalid or improperly formatted."),
 	SEARCH_EXECUTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SEARCH_500_1", "Error occurred while executing search query.");
 	private final HttpStatus status;
 	private final String code;
