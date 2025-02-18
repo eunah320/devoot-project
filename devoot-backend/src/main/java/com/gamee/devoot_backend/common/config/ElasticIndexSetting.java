b28a8e2e73cca310ef7e64908bfe4654c365d735
권정우 <kiw970923@naver.com>
2025-02-18 17:23:28 +0900

    FIX: es-setting.json 읽어올 때 UTF-8 인코딩 지정

--- devoot-backend/src/main/java/com/gamee/devoot_backend/common/config/ElasticIndexSetting.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/common/config/ElasticIndexSetting.java
@@ -3,7 +3,9 @@ package com.gamee.devoot_backend.common.config;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 
 import jakarta.annotation.PostConstruct;
 
@@ -45,9 +47,11 @@ public class ElasticIndexSetting {
 			if (inputStream == null) {
 				throw new FileNotFoundException("es-setting.json not found in classpath");
 			}
-			JsonNode data = objectMapper.readTree(inputStream);
-			createIndex(indexOperations.getIndexCoordinates().getIndexName(), data.toString());
-			System.out.println("Index created with settings and mappings!");
+			try (InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+				var data = objectMapper.readTree(reader);
+				createIndex(indexOperations.getIndexCoordinates().getIndexName(), data.toString());
+				System.out.println("Index created with settings and mappings!");
+			}
 		} else {
 			System.out.println("Index already exists, skipping creation");
 		}
