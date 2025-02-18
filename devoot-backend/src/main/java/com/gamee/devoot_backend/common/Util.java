58f6f781a743c1cfedef4a6d1115dcddf802a9e9
SSAFY <spig0126@naver.com>
2025-02-18 11:31:30 +0900

    FIX: Util sha256 hash function

--- devoot-backend/src/main/java/com/gamee/devoot_backend/common/Util.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/common/Util.java
@@ -1,5 +1,8 @@
 package com.gamee.devoot_backend.common;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -24,7 +27,21 @@ public class Util {
 		}
 	}
 
-	public static String generateHash(String content) {
-		return encoder.encode(content).replace("{SHA-256}", "");
+	public static String sha256(String message) {
+		if (message == null)
+			return null;
+		MessageDigest sha;
+		String result = "";
+		try {
+			sha = MessageDigest.getInstance("SHA-256");
+			sha.update(message.getBytes());
+			for (byte b : sha.digest()) {
+				result += Integer.toHexString(b & 0xff);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return result;
 	}
 }
