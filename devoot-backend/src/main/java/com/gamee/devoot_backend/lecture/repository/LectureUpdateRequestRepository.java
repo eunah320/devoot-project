578d28fabb472eb00311952459bbacbfb08b6f49
SSAFY <spig0126@naver.com>
2025-02-19 14:21:28 +0900

    FIX: merge conflicts
    jira-ref: #S12P11A209-305

--- devoot-backend/src/main/java/com/gamee/devoot_backend/lecture/repository/LectureUpdateRequestRepository.java
+++ devoot-backend/src/main/java/com/gamee/devoot_backend/lecture/repository/LectureUpdateRequestRepository.java
@@ -8,4 +8,6 @@ import com.gamee.devoot_backend.lecture.entity.LectureUpdateRequest;
 
 public interface LectureUpdateRequestRepository extends JpaRepository<LectureUpdateRequest, Long> {
 	Optional<LectureUpdateRequest> findByLectureId(Long lectureId);
+
+	void deleteByLectureId(Long lectureId);
 }
